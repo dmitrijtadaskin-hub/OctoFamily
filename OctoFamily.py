506a33386c1578fe839ac0548c8f840a94503df8
Dmitriy Tadashkin <dmitrijtadaskin@gmail.com>
2025-10-05 13:44:51 +0300

    Test

---
+++ OctoFamily.py
@@ -0,0 +1,13 @@
+import random
+
+octocat_options = [
+    "Octocat_Original",
+    "Octocat_Mommy",
+    "Octocat_Blue",
+    "Octocat_Red"
+    "Octocat_Daddy"
+]
+
+selected_octocat = random.choice(octocat_options)
+
+print(f"Your Octocat: {selected_octocat}")
